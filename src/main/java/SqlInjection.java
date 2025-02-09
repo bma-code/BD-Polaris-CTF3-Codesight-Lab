27d8d78f836a9adcb4bf64d66e16f498b5929a28
Jason Phillips <pjason@blackduck.com>
2025-02-09 13:47:58 -0500

    Update SqlInjection.java

--- src/main/java/SqlInjection.java
+++ src/main/java/SqlInjection.java
@@ -3,8 +3,9 @@ import javax.servlet.http.HttpServletRequest;
 
 public class SqlInjection {
     public static ResultSet doQuery(HttpServletRequest request, Connection connection) throws SQLException {
-        // BAD CODE
         String customerName = request.getParameter("customerName");
+        
+        // BAD CODE
         String query = "SELECT account_balance FROM user_data WHERE user_name = " + customerName;
         Statement statement = connection.createStatement();
         ResultSet results = statement.executeQuery(query);
