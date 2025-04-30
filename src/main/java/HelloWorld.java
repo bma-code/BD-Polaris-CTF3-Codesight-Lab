4177cb00c835b75845efc7b7214ffc2a8c0420dd
Jason Phillips <pjason@blackduck.com>
2025-04-30 09:25:03 -0400

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -6,6 +6,7 @@ import org.apache.logging.log4j.Logger;
 public class HelloWorld {
     private static final Logger logger = LogManager.getLogger("HelloWorld");
     public static void main(String[] args) {
+        String secret = "QnkgQmxhY2sgRHVjaw==";
         logger.info("Hello, World!");
     }
 }
